a838f33b8eb84f72757f4a3b660fc0f8c8a6492c
hadar <hadar424@gmail.com>
2019-05-27 17:36:49 +0300

    Add project files.

---
+++ Ex3/Controllers/FirstController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex3.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index(string ip, string myPort)
+        {
+            ViewBag.ip = ip;
+            ViewBag.port = myPort;
+            return View();
+        }
+    }
+}

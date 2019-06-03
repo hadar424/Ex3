31d1e9675ffb7e7e35ba864734887399d8e3b1a4
sharonshiri <sharonshiri@gmail.com>
2019-06-03 17:43:23 +0300

    sharon&hadar

--- Ex3/Controllers/FirstController.cs
+++ Ex3/Controllers/FirstController.cs
@@ -262,9 +262,8 @@ namespace Ex3.Controllers
                 return data;
             }
             // get the values
-            Random rnd = new Random();
-            float lon = getData(data, 0) + rnd.Next(50);
-            float lat = getData(data, 1) + rnd.Next(50);
+            float lon = getData(data, 0);
+            float lat = getData(data, 1);
             float throttle = getData(data, 2);
             float rudder = getData(data, 3);
 
